13ac4d4b3447eaf11bccb4e344cc001d76a3f1db
YegorChevardin <egor03052004@gmail.com>
2023-09-13 17:07:42 +0200

    Initial commit

---
+++ src/main/java/com/yehorchevardin/andersen/git/App.java
@@ -0,0 +1,13 @@
+package com.yehorchevardin.andersen.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
